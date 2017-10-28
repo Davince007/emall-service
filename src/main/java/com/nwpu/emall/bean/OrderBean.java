386ac69a4d7f1e7dcdf7ff780f56756c1b59f179
GWD <gaoweidong127@163.com>
2017-10-29 06:11:05 +0800

    添加订单信息类

---
+++ src/main/java/com/nwpu/emall/bean/OrderBean.java
@@ -0,0 +1,19 @@
+package com.nwpu.emall.bean;
+/**
+ * 订单信息
+ * @author GWD
+ *
+ */
+public class OrderBean {
+
+	private String orderId;
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	
+}
